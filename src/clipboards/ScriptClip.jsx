adc58caa49fd8570aa74b1a510dd0b27a5c64dc3
meltonsilvayotpo <melton.silva@yotpo.com>
2025-08-22 15:11:07 +0800

    asd

--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/QC6DbgcHis6emNQcVCfV8oQfjBfHCABFztTzIpU8?languageCode=ko" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/QC6DbgcHis6emNQcVCfV8oQfjBfHCABFztTzIpU8" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
